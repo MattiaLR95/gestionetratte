13958c26a87f48c30ceb6caaff1f4d09df5bb692
MattiaLR95 <laroccamattia@virgilio.it>
2022-11-26 19:10:38 +0100

    Implementati Crud di Airbus e Tratta

--- src/main/java/it/prova/gestionetratte/web/api/AirbusController.java
+++ src/main/java/it/prova/gestionetratte/web/api/AirbusController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,15 +43,33 @@ public class AirbusController {
 
 		return AirbusDTO.buildAirbusDTOFromModel(airbus, true);
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public AirbusDTO createNew(@Valid @RequestBody AirbusDTO airbusInput) {
-		if(airbusInput.getId() != null)
+		if (airbusInput.getId() != null)
 			throw new IdNotNullForInsertException("Non è ammesso fornire un id per la creazione di un nuovo Airbus");
-		
+
 		Airbus airbusInserito = airbusService.inserisciNuovo(airbusInput.buildAirbusModel());
 		return AirbusDTO.buildAirbusDTOFromModel(airbusInserito, false);
+	}
+
+	@PutMapping
+	public AirbusDTO update(@Valid @RequestBody AirbusDTO airbusInput, @PathVariable(required = true) Long id) {
+		Airbus airbus = airbusService.caricaSingoloElemento(id);
+
+		if (airbus == null)
+			throw new AirbusNotFoundException("Regista not found with id " + id);
+
+		airbusInput.setId(id);
+		Airbus airbusAggiornato = airbusService.aggiorna(airbusInput.buildAirbusModel());
+		return AirbusDTO.buildAirbusDTOFromModel(airbusAggiornato, false);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(required = true) Long id) {
+		airbusService.rimuovi(id);
 	}
 
 }
