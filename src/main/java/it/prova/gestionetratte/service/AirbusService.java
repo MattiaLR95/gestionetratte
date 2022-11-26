fce5b96b38098d823db301791f4910fda5d0f41a
MattiaLR95 <laroccamattia@virgilio.it>
2022-11-26 12:46:31 +0100

    Implementati AirbusService e le classi Exception

---
+++ src/main/java/it/prova/gestionetratte/service/AirbusService.java
@@ -0,0 +1,26 @@
+package it.prova.gestionetratte.service;
+
+import java.util.List;
+
+import it.prova.gestionetratte.model.Airbus;
+
+public interface AirbusService {
+
+	List<Airbus> listAllElements();
+
+	List<Airbus> listAllElementsEager();
+
+	Airbus caricaSingoloElemento(Long id);
+
+	Airbus caricaSingoloElementoConTratte(Long id);
+
+	Airbus aggiorna(Airbus airbusInstance);
+
+	Airbus inserisciNuovo(Airbus airbusInstance);
+
+	void rimuovi(Long idToRemove);
+
+	List<Airbus> cercaByCodiceEDescrizioneILike(String codice, String descrizione);
+
+	Airbus findByCodiceAndDescrizione(String codice, String descrizione);
+}
---
+++ src/main/java/it/prova/gestionetratte/service/AirbusServiceImpl.java
@@ -0,0 +1,70 @@
+package it.prova.gestionetratte.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.prova.gestionetratte.model.Airbus;
+import it.prova.gestionetratte.repository.airbus.AirbusRepository;
+import it.prova.gestionetratte.web.api.exception.AirbusNotFoundException;
+
+@Service
+public class AirbusServiceImpl implements AirbusService {
+
+	@Autowired
+	AirbusRepository repository;
+
+	@Override
+	public List<Airbus> listAllElements() {
+		return (List<Airbus>) repository.findAll();
+	}
+
+	@Override
+	public List<Airbus> listAllElementsEager() {
+		return (List<Airbus>) repository.findAllEager();
+	}
+
+	@Override
+	public Airbus caricaSingoloElemento(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Airbus caricaSingoloElementoConTratte(Long id) {
+		return repository.findByIdEager(id);
+	}
+
+	@Override
+	@Transactional
+	public Airbus aggiorna(Airbus airbusInstance) {
+		return repository.save(airbusInstance);
+	}
+
+	@Override
+	@Transactional
+	public Airbus inserisciNuovo(Airbus airbusInstance) {
+		return repository.save(airbusInstance);
+	}
+
+	@Override
+	public void rimuovi(Long idToRemove) {
+		repository.findById(idToRemove)
+				.orElseThrow(() -> new AirbusNotFoundException("Airbus not found con id: " + idToRemove));
+		repository.deleteById(idToRemove);
+	}
+
+	@Override
+	public List<Airbus> cercaByCodiceEDescrizioneILike(String codice, String descrizione) {
+		return repository.findByDescrizioneIgnoreCaseContainingOrCodiceIgnoreCaseContainingOrderByCodiceAsc(codice,
+				descrizione);
+	}
+
+	@Override
+	public Airbus findByCodiceAndDescrizione(String codice, String descrizione) {
+		return repository.findByCodiceAndDescrizione(codice, descrizione);
+	}
+
+}
